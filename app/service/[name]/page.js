c8091f95420e00189fef36395ac59e4bc40334f2
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-07-03 12:39:53 +0530

    fix: remove unused useParams import and related code from page component

--- app/service/[name]/page.js
+++ app/service/[name]/page.js
@@ -1,11 +1,8 @@
 "use client"
 import React from 'react'
-import { useParams } from 'next/navigation'
 import style from "../../style/service.module.css"
 
 function page() {
-  const params = useParams()
-  // console.log(params)
   return (
     <>
     <div className={style.service_page}>
