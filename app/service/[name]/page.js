debde5339afe4469cc4d20c2e576d744ca65733e
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-07-03 12:37:56 +0530

    chore: update code structure and remove redundant changes

--- app/service/[name]/page.js
+++ app/service/[name]/page.js
@@ -1,8 +1,23 @@
+"use client"
 import React from 'react'
+import { useParams } from 'next/navigation'
+import style from "../../style/service.module.css"
 
 function page() {
+  const params = useParams()
+  // console.log(params)
   return (
-    <div>Navbar dynamic page</div>
+    <>
+    <div className={style.service_page}>
+      <div  className={style.service_bg}>
+      <div  className={style.service_bg_orange}></div>
+      <div className={style.service_content}>
+        <h1>Modern Web Design: Trends, Tools & Timeless Principles</h1>
+        <img src="/service.png" alt=''/>
+      </div>
+      </div>
+    </div>
+   </> 
   )
 }
 
