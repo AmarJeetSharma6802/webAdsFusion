5ee56bf191d2ca2039db644093084cddb31dd37c
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-07-03 21:14:31 +0530

    fix: add missing paragraph for clarity in service page content

--- app/service/[name]/page.js
+++ app/service/[name]/page.js
@@ -10,6 +10,7 @@ function page() {
       <div  className={style.service_bg_orange}></div>
       <div className={style.service_content}>
         <h1>Modern Web Design: Trends, Tools & Timeless Principles</h1>
+        <p>How to Design a Website From Scratch in 2025Web design is a comprehensive process encompassing the planning, conceptualization, and implementation of a website's visual layout and user experience.</p>
         <img src="/serviceWebDesign.png" alt='' style={{width:"50%"}}/>
       </div>
       </div>
