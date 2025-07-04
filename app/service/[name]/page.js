ef676d16d519eebe7bf8847f0f37f184d77be78e
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-07-04 10:03:49 +0530

    fix: wrap heading and paragraph in a div for better structure in service page

--- app/service/[name]/page.js
+++ app/service/[name]/page.js
@@ -9,8 +9,10 @@ function page() {
       <div  className={style.service_bg}>
       <div  className={style.service_bg_orange}></div>
       <div className={style.service_content}>
-        <h1>Modern Web Design: Trends, Tools & Timeless Principles</h1>
-        <p>How to Design a Website From Scratch in 2025Web design is a comprehensive process encompassing the planning conceptualization and implementation of a website{"'"}s visual layout and user experience.</p>
+      <div>
+          <h1>Modern Web Design: Trends, Tools & Timeless Principles</h1>
+        <p className={style.service_para}>How to Design a Website From Scratch in 2025Web design is a comprehensive process encompassing the planning conceptualization and implementation of a website{"'"}s visual layout and user experience.</p>
+      </div>
         <img src="/serviceWebDesign.png" alt='' style={{width:"50%"}}/>
       </div>
       </div>
