a4da3e8f568d86ea241810d5f91d79d2ca33aa6d
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-07-11 10:26:40 +0530

    feat: implement authentication middleware with JWT token verification

---
+++ app/api/middleware/auth.middleware.js
@@ -0,0 +1,45 @@
+import jwt from "jsonwebtoken";
+import signInUser from "../model/signIn.model";
+import { NextResponse } from "next/server";
+
+async function authUser(req) {
+  try {
+    const token =
+      req.cookies.get("accessToken")?.value ||
+      req.headers.get("authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      return {
+        error: NextResponse.json({ message: "Unauthorized" }, { status: 401 }),
+      };
+    }
+
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN);
+    if (!decoded) {
+      return {
+        error: NextResponse.json({ message: "Unauthorized" }, { status: 401 }),
+      };
+    }
+    const user = await signInUser
+      .findById(decoded.user_id)
+      .select("-password -refreshToken -__v");
+
+    if (!user) {
+      return {
+        error: NextResponse.json(
+          { message: "User not found" },
+          { status: 404 }
+        ),
+      };
+    }
+
+    return { user };
+  } catch (error) {
+    return {
+      error: NextResponse.json(
+        { message: "Internal Server Error" },
+        { status: 500 }
+      ),
+    };
+  }
+}
