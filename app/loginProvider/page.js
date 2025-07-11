90d1a7380e5db330f10ea39f2d2338ae96a56b6b
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-07-11 12:36:51 +0530

    loginProvider add

---
+++ app/loginProvider/page.js
@@ -0,0 +1,28 @@
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function LoginButton() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div>
+        <p>Welcome, {session.user.name}</p>
+        <p>Email: {session.user.email}</p>
+        <img
+          src={session.user.image}
+          alt="User Avatar"
+          width={50}
+          height={50}
+          style={{ borderRadius: "50%" }}
+        />
+        <button onClick={() => signOut()}>Sign Out</button>
+      </div>
+    );
+  }
+
+  return (
+    <button onClick={() => signIn("google")}>Sign in with Google</button>
+  );
+}
