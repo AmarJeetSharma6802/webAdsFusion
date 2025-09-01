53743824a1e61f85fcf23bd8e1d6f9c519303f3c
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-09-01 19:14:19 +0530

    `Removed useParams and console.log from page.js`

--- app/blogs/[read]/page.js
+++ app/blogs/[read]/page.js
@@ -1,13 +1,9 @@
-"use client"
 import React from 'react'
-import { useParams } from 'next/navigation'
 
 function page() {
-  const Params = useParams()
-console.log(Params)
   return (
     <div>
-      <p>{Params.read}</p>
+      <p></p>
     </div>
   )
 }
