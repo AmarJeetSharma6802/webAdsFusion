c5e0fd822422d150e02042d4656a69465d48f2f7
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-09-01 19:37:46 +0530

    `feat: updated blog API and component to include timestamps and pagination`

--- app/api/model/blog.model.js
+++ app/api/model/blog.model.js
@@ -25,7 +25,7 @@ const blogSchema = new mongoose.Schema(
     },
   },
   {
-    timeStamp: true,
+     timestamps: true,
   }
 );
 
--- app/blogs/Blog.jsx
+++ app/blogs/Blog.jsx
@@ -5,45 +5,50 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 function Blog() {
-  const router = useRouter()
+  const router = useRouter();
   const [dt, setDt] = useState([]);
   const [page, setPage] = useState(1);
   const blogsPerPage = 6;
   const blogRef = useRef(null);
 
+  // Fetch data from API
   useEffect(() => {
     const fetchData = async () => {
-     const res = await fetch("/api/blog", {
-    next: { revalidate: 10 } 
-  });
+      const res = await fetch("/api/blog", { next: { revalidate: 10 } });
       const data = await res.json();
-      setDt(data.foundblog); 
+      
+      // Latest blog first
+      const sortedBlogs = [...data.foundblog].reverse(); // newest first
+      setDt(sortedBlogs);
     };
     fetchData();
   }, []);
 
+  // Scroll to blog list
   const handleScroll = () => {
     blogRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   // Current page blogs
   const indexOfLastBlog = page * blogsPerPage;
-  const indexOfFirstBlog = indexOfLastBlog - blogsPerPage;
+  const indexOfFirstBlog = (page - 1) * blogsPerPage;
   const currentBlogs = dt.slice(indexOfFirstBlog, indexOfLastBlog);
 
   // Total pages
   const totalPages = Math.ceil(dt.length / blogsPerPage);
 
-  // Click on page number
+  // Page click
   const handlePageClick = (pageNum) => {
     setPage(pageNum);
     handleScroll();
   };
 
-  const handleClick =(heading)=>{
-    const slug = heading.toLowerCase().replace(/\s+/g, "-")
-    router.push(`/blogs/${slug}`)
-  }
+  // Navigate to blog details page
+  const handleClick = (heading) => {
+    const slug = heading.toLowerCase().replace(/\s+/g, "-");
+    router.push(`/blogs/${slug}`);
+  };
+
   return (
     <div>
       <div className={style.blog_head}>
@@ -58,27 +63,32 @@ function Blog() {
         </div>
       </div>
 
-<div className={style.blog_box}>
-  <h1  className={style.blog_box_heading}>Featured Articles</h1>
-      <div className={style.blog} ref={blogRef}>
-        {currentBlogs.map((item) => (
-          <div key={item._id} className={style.blog_key} onClick={()=> handleClick(item.heading)}>
-            <div className={style.blog_wrap}>
-              <Image src={item.image} alt={item.heading} width={200} height={100} className={style.blog_image} />
+      <div className={style.blog_box}>
+        <h1 className={style.blog_box_heading}>Featured Articles</h1>
+        <div className={style.blog} ref={blogRef}>
+          {currentBlogs.map((item) => (
+            <div key={item._id} className={style.blog_key} onClick={() => handleClick(item.heading)}>
+              <div className={style.blog_wrap}>
+                <Image
+                  src={item.image}
+                  alt={item.heading}
+                  width={200}
+                  height={100}
+                  className={style.blog_image}
+                />
+              </div>
+              <div className={style.blog_details}>
+                <h3 className={style.blog_details_heading}>{item.heading}</h3>
+                <p className={style.blog_details_title}>{item.title}</p>
+                <div className={style.btn_arrow}>
+                  <p className={style.blog_details_btn}>Read more</p>
+                  <p><i className="fa-solid fa-arrow-right"></i></p>
+                </div>
+              </div>
             </div>
-           <div className={style.blog_details}>
-               <h3 className={style.blog_details_heading}  >{item.heading}</h3>
-            <p className={style.blog_details_title}>{item.title}</p>
-             <div className={style.btn_arrow} >
-            <p  className={style.blog_details_btn}>Read more</p>
-            <p ><i className="fa-solid fa-arrow-right"></i></p>
-            </div>
-           </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-
-</div>
 
       {/* Page Numbers */}
       <div className={style.pageNumber}>
@@ -86,7 +96,7 @@ function Blog() {
           <button
             key={num}
             onClick={() => handlePageClick(num)}
-            className={`${style.pageNumber_btn} ${ num === page ? style.active :""}`}
+            className={`${style.pageNumber_btn} ${num === page ? style.active : ""}`}
           >
             {num}
           </button>
