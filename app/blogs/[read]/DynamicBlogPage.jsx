46fc8cadd37108c4b9cd380a3fbd9a23fd56193a
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-09-01 20:23:47 +0530

    `Refactor DynamicBlogPage.jsx and page.js to remove unused imports and update function signatures`

--- app/blogs/[read]/DynamicBlogPage.jsx
+++ app/blogs/[read]/DynamicBlogPage.jsx
@@ -1,20 +1,18 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { useParams } from "next/navigation";
 import Image from "next/image";
 
 
-export default function BlogDetailPage() {
-  const params = useParams();
+export default function BlogDetailPage({read}) {
   const [blog, setBlog] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  const { read } = params; 
   
   useEffect(() => {
     const fetchBlog = async () => {
       const res = await fetch("/api/blog", { next: { revalidate: 10 } });
       const data = await res.json();
+
 
       const slugify = (heading) =>
         heading.toLowerCase().replace(/\s+/g, "-");
@@ -36,13 +34,7 @@ export default function BlogDetailPage() {
     <div>
       <h1>{blog.heading}</h1>
       <p>{blog.blogType}</p>
-      <p>
-        {new Date(blog.createdAt).toLocaleDateString("en-US", {
-          month: "long",
-          day: "numeric",
-          year: "numeric",
-        })}
-      </p>
+      
       <Image src={blog.image} alt={blog.heading} width={600} height={300} />
       <h3>{blog.title}</h3>
       <p>{blog.LongPara}</p>
--- app/blogs/[read]/page.js
+++ app/blogs/[read]/page.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import BlogDetailPage from "../[read]/DynamicBlogPage"
-function page() {
-  
+import BlogDetailPage from "../[read]/DynamicBlogPage";
+
+// Page component
+export default function Page({ params }) {
+
+  const { read } = params;
+
   return (
     <div>
-      <BlogDetailPage/>
+      <BlogDetailPage read={read} />
     </div>
-  )
+  );
 }
 
-export default page
+// Metadata generate karna
+export async function generateMetadata({ params }) {
+  const { read } = params;
+  const slugToTitle = read
+    .replace(/-/g, " ")
+    .replace(/\b\w/g, (char) => char.toUpperCase());
+
+  return {
+    title: `${slugToTitle} | Creative Digital Marketing & Web Development Agency in India`,
+    description: `Read about ${slugToTitle} on our blog.`,
+    keywords: `${slugToTitle}, Web Ads Fusion, Digital Services, Web Development, Web Design`,
+  };
+}
