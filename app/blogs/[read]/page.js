4a6d88d1358cecd243be27ba3527becec2f2403d
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-09-02 20:24:29 +0530

    `Refactor DynamicBlogPage.jsx to fetch blog data from API and display blog content`

--- app/blogs/[read]/page.js
+++ app/blogs/[read]/page.js
@@ -1,25 +1,13 @@
 import BlogDetailPage from "../[read]/DynamicBlogPage";
 
 // Page component
-export default async function Page({ params }) {
+export default function Page({ params }) {
 
   const { read } = params;
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blog`, {
-    next: { revalidate: 10 }, 
-  });
-  const data = await res.json();
-
-  const slugify = (heading) =>
-    heading.toLowerCase().replace(/\s+/g, "-");
-
-const blog = data.foundblog.find(
-    (item) => slugify(item.heading) === read
-  );
-
   return (
     <div>
-      <BlogDetailPage blog={blog} />
+      <BlogDetailPage read={read} />
     </div>
   );
 }
