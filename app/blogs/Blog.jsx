722b952d11b0c05918f599af3ce467a13488a3ca
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-09-02 19:20:12 +0530

    `Refactored Blog component to use BlogsList component and simplified API call`

--- app/blogs/Blog.jsx
+++ app/blogs/Blog.jsx
@@ -1,110 +1,35 @@
-"use client";
-import React, { useEffect, useState, useRef } from "react";
+import BlogsList from "../blogs/BlogSec.jsx"; 
 import style from "../style/blog.module.css";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
 
-function Blog() {
-  const router = useRouter();
-  const [dt, setDt] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState(1);
-  const blogsPerPage = 6;
-  const blogRef = useRef(null);
+async function getBlogs() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blog`, {
+    next: { revalidate: 10 }, 
+  });
+  return res.json();
+}
 
-  // Fetch data from API
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("/api/blog", { next: { revalidate: 10 } });
-      const data = await res.json();
-      
-      const sortedBlogs = [...data.foundblog].reverse(); 
-      setDt(sortedBlogs);
-    };
-    fetchData();
-    setLoading(false)
-  }, [loading,dt]);
+export default async function Blog() {
+  const data = await getBlogs();
+  const blogs = [...data.foundblog].reverse(); 
 
-  const handleScroll = () => {
-    blogRef.current?.scrollIntoView({ behavior: "smooth" });
-  };
-
-  // Current page blogs
-  const indexOfLastBlog = page * blogsPerPage;
-  const indexOfFirstBlog = (page - 1) * blogsPerPage;
-  const currentBlogs = dt.slice(indexOfFirstBlog, indexOfLastBlog);
-
-  const totalPages = Math.ceil(dt.length / blogsPerPage);
-
-  const handlePageClick = (pageNum) => {
-    setPage(pageNum);
-    handleScroll();
-  };
-
-  // Navigate to blog details page
-  const handleClick = (heading) => {
-    const slug = heading.toLowerCase().replace(/\s+/g, "-");
-    router.push(`/blogs/${slug}`);
-  };
-
-  if(loading){
-    return <p className={style.loading}>loading ....</p>
-  }
   return (
     <div>
       <div className={style.blog_head}>
         <div className={style.blog_head_details}>
-          <p className={style.blog_head_details_heading}>Explore Ideas That Inspire</p>
+          <p className={style.blog_head_details_heading}>
+            Explore Ideas That Inspire
+          </p>
           <p className={style.blog_head_details_para}>
             Insights, strategies and news about digital advertising, ad tech, and marketing trends
           </p>
-          <button className={style.blog_head_details_btn} onClick={handleScroll}>
-            Start Reading
-          </button>
+          <a href="#blogs">
+            <button className={style.blog_head_details_btn}>Start Reading</button>
+          </a>
         </div>
       </div>
 
-      <div className={style.blog_box}>
-        <h1 className={style.blog_box_heading}>Featured Articles</h1>
-        <div className={style.blog} ref={blogRef}>
-          {currentBlogs.map((item) => (
-            <div key={item._id} className={style.blog_key} onClick={() => handleClick(item.heading)}>
-              <div className={style.blog_wrap}>
-                <Image
-                  src={item.image}
-                  alt={item.heading}
-                  width={200}
-                  height={100}
-                  className={style.blog_image}
-                />
-              </div>
-              <div className={style.blog_details}>
-                <h3 className={style.blog_details_heading}>{item.heading}</h3>
-                <p className={style.blog_details_title}>{item.title}</p>
-                <div className={style.btn_arrow}>
-                  <p className={style.blog_details_btn}>Read more</p>
-                  <p><i className="fa-solid fa-arrow-right"></i></p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* Page Numbers */}
-      <div className={style.pageNumber}>
-        {Array.from({ length: totalPages }, (_, i) => i + 1).map((num) => (
-          <button
-            key={num}
-            onClick={() => handlePageClick(num)}
-            className={`${style.pageNumber_btn} ${num === page ? style.active : ""}`}
-          >
-            {num}
-          </button>
-        ))}
-      </div>
+      {/* pagination */}
+      <BlogsList blogs={blogs} />
     </div>
   );
 }
-
-export default Blog;
---
+++ app/blogs/BlogSec.jsx
@@ -0,0 +1,105 @@
+"use client";
+import { useState, useRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import style from "../style/blog.module.css";
+import { useRouter } from "next/navigation";
+
+export default function BlogsList({ blogs }) {
+
+    const router = useRouter()
+  const [page, setPage] = useState(1);
+  const blogsPerPage = 6;
+  const blogRef = useRef(null);
+
+  const totalPages = Math.ceil(blogs.length / blogsPerPage);
+
+  const indexOfLastBlog = page * blogsPerPage;
+  const indexOfFirstBlog = (page - 1) * blogsPerPage;
+  const currentBlogs = blogs.slice(indexOfFirstBlog, indexOfLastBlog);
+
+  const handlePageClick = (pageNum) => {
+    setPage(pageNum);
+    blogRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const handleNext = () => {
+    if (page < totalPages) {
+      setPage(page + 1);
+      blogRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const handlePrev = () => {
+    if (page > 1) {
+      setPage(page - 1);
+      blogRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const handleBlog = (heading)=>{
+    const sulg  = heading.toLowerCase().replace(/\s+/g, "-")
+    router.push(`/blogs/${sulg}`)
+  }
+  return (
+    <div className={style.blog_box} id="blogs">
+      <h1 className={style.blog_box_heading}>Featured Articles</h1>
+      <div className={style.blog} ref={blogRef}>
+        {currentBlogs.map((item) => (
+          <div key={item._id} className={style.blog_key} onClick={()=>handleBlog(item.heading)}>
+            <div className={style.blog_wrap}>
+              <Image
+                src={item.image}
+                alt={item.heading}
+                width={200}
+                height={100}
+                className={style.blog_image}
+              />
+            </div>
+            <div className={style.blog_details}>
+              <h3 className={style.blog_details_heading}>{item.heading}</h3>
+              <p className={style.blog_details_title}>{item.title}</p>
+              <div className={style.btn_arrow}>
+                <Link
+                  href={`/blogs/${item.heading.toLowerCase().replace(/\s+/g, "-")}`}
+                >
+                  <p className={style.blog_details_btn}>Read more</p>
+                </Link>
+                <p><i className="fa-solid fa-arrow-right"></i></p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* Pagination Controls */}
+      <div className={style.pageNumber}>
+        <button
+          onClick={handlePrev}
+          disabled={page === 1}
+          className={style.pageNumber_btn}
+        >
+          Prev
+        </button>
+
+        {Array.from({ length: totalPages }, (_, i) => i + 1).map((num) => (
+          <button
+            key={num}
+            onClick={() => handlePageClick(num)}
+            className={`${style.pageNumber_btn} ${num === page ? style.active : ""}`}
+          >
+            {num}
+          </button>
+        ))}
+
+        <button
+          onClick={handleNext}
+          disabled={page === totalPages}
+          className={style.pageNumber_btn}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
