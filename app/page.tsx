f29a9377519123c6b7d477ae632c4fe5273e5271
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-12-14 21:02:20 +0530

    Update video source URL in Home component and change JSX setting in tsconfig

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
         <div className={style.home_video_bg}>
           <video
           className={style.home_video}
-            src="https://res.cloudinary.com/futurecoder/video/upload/v1750313516/w7tkh3dgwinnsz7zrhtv.mp4"
+            src="https://res.cloudinary.com/futurecoder/video/upload/v1765726282/yvz95cpnhjrsnzyswmgn.mp4"
             loop
             muted
             autoPlay
